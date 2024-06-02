7192b6b282dcd225a6606c84e21a252bf320c46c
Satyaki-Roy <satyaki.roy@powerschool.com>
2024-06-02 19:40:35 +0530

    functional component

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const heading = (
+  <h1 className="heading">Hello World</h1>
+);
+
+// functional component
+const HeadingComponent = () => (
+  <h1 className="heading">Hello World</h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading = React.createElement("h1", { id: "heading" }, "Hello World");
-root.render(heading);
+root.render(<HeadingComponent />);
